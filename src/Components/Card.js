29261e3dce63f1b3f5a5c6eff659c30ae8c05de7
darlson <davertiser@gmail.com>
2020-05-15 02:53:18 -0600

    completed project

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -3,17 +3,17 @@ import React from 'react'
 
 export default function Card ({person}) {
         return (
-            <div>
+            <div className='card'>
                 <div className='card-right'>
                     <h2>{person.id}/25</h2>
                 </div>
                 <div className='card-left'>
-                    <h1>{person.name.first} {person.name.last}</h1>
-                    <h3><strong>From: </strong>{person.city}, {person.country}</h3>
-                    <h3><strong>Job Title: </strong>{person.title}</h3>
-                    <h3><strong>Employer: </strong>{person.employer}</h3>
+                    <h2>{person.name.first} {person.name.last}</h2>
+                    <h4><span>From: </span>{person.city}, {person.country}</h4>
+                    <h4><span>Job Title: </span>{person.title}</h4>
+                    <h4><span>Employer: </span>{person.employer}</h4>
                     <br />
-                    <h3><strong>Favorite Movies: </strong></h3>
+                    <h4><span>Favorite Movies: </span></h4>
                     <ol>
                         <li>{person.favoriteMovies[0]}</li>
                         <li>{person.favoriteMovies[1]}</li>
